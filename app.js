c941d0a8600471088acc7ff275b5322564ca9759
MiguelAngelValle <79349697+MiguelAngelValle@users.noreply.github.com>
2021-11-11 19:40:50 -0600

    add

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+const fs = require('fs');
+const express = require("express");
+const cors = require('cors')
+const app = express();
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+const port = process.env.PORT || 3000;
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+//load 
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(cors());
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
-});
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+app.listen(port, () => {
+    console.log(`App listening at PORT:${port}`);
+})
